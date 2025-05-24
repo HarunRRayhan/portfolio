7080ef855c4d19a3b77d98e3e48ae629886ce2ca
Harun R. Rayhan <harun.b13@gmail.com>
2025-05-24 12:18:02 +0600

    wip

--- docker/entrypoint-nginx.sh
+++ docker/entrypoint-nginx.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 set -e
 
-echo "[entrypoint-nginx.sh] Starting as $(whoami)"
+echo "[entrypoint-nginx.sh] Starting nginx as $(whoami)"
 
-# Ensure required directories exist
-mkdir -p storage bootstrap/cache
+# Ensure nginx can write to its required directories
+mkdir -p /tmp/nginx /var/cache/nginx || true
 
-# No chown for nginx
-chmod -R 775 storage bootstrap/cache
-
+# Start nginx in foreground
 exec nginx -g 'daemon off;' 
