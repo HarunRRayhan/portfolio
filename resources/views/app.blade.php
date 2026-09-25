b8119c583940e055f30c39c61dfd1c7cd65166dd
Harun R Rayhan <harun.b13@gmail.com>
2026-09-25 12:45:44 +0600

    Self-host critical fonts and prevent SSR email rewriting (#192)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,17 +26,21 @@
         <link rel="manifest" href="/site.webmanifest">
 
         <!-- Optional fonts keep slow downloads from shifting already-painted text. -->
+        @if (! app(\Illuminate\Foundation\Vite::class)->isRunningHot())
+            <link rel="preload" as="font" type="font/woff2" crossorigin
+                href="{{ Vite::asset('node_modules/@fontsource-variable/inter/files/inter-latin-wght-normal.woff2') }}">
+        @endif
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link
             rel="preload"
             as="style"
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Fraunces:opsz,wght@9..144,400;9..144,500;9..144,600;9..144,700&family=JetBrains+Mono:wght@400;500;600;700&family=Hind+Siliguri:wght@400;500;600;700&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,400;9..144,500;9..144,600;9..144,700&family=Hind+Siliguri:wght@400;500;600;700&display=optional"
             onload="this.onload=null;this.rel='stylesheet'"
         >
         <noscript>
             <link
-                href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Fraunces:opsz,wght@9..144,400;9..144,500;9..144,600;9..144,700&family=JetBrains+Mono:wght@400;500;600;700&family=Hind+Siliguri:wght@400;500;600;700&display=optional"
+                href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,400;9..144,500;9..144,600;9..144,700&family=Hind+Siliguri:wght@400;500;600;700&display=optional"
                 rel="stylesheet"
             >
         </noscript>
@@ -91,6 +95,8 @@
         @endif
     </head>
     <body class="font-sans antialiased">
+        <!--email_off-->
         @inertia
+        <!--/email_off-->
     </body>
 </html>
